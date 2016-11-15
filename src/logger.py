e39924f62396b7306b7678efa2c5ebf73dd263e0
Daniel Bargmann <daniel.bargmann@ipa.fraunhofer.de>
2016-11-15 19:08:35 +0100

    rewritten logger to be used with ressource-managers, added forcedump of data at the end of logging and variable bufferSize and logfile names/directory

--- src/logger.py
+++ src/logger.py
@@ -1,31 +1,97 @@
-#!/usr/bin/env python
+"""
+.. module:: logger
+    :platform: Unix
+    :synopsis: A module containing
+.. moduleauthoer:: Daniel Bargmann <daniel.bargmann@ipa.fraunhofer.de>
+
+"""
+
 import rospy
 from math import *
 import time
 import csv
+import sys
 from myo_msgs.msg import statusMessage
 
+'''
+time     time
+float64  position
+float64  velocity
+float64  displacement
+float64  reference
+float64  current
+float64  commanded_effort
+float64  analogIN0
+int32    clutchState
+'''
+
 class simpleLogger:
-    def __init__(self):
+    """Simple Logger class
+
+    This class is a simple logger, which takes
+
+    Attributes
+    ----------
+    fileName : string
+    bufferSize : int
+    count : int
+    dataSize : int
+    buffer : array
+
+
+
+    """
+
+    def __init__(self,name, bufferSize=1000, rosNode="/myo/myo_muscle0_controller/DebugMessage"):
+        """
+
+        """
         # create filename from date
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        self.fileName = "data-" + timestr
+        try:
+            os.mkdir("data")
+        except Exception:
+            pass
+        self.fileName =  "data/" + name + "-" + timestr
         with open(self.fileName,'wb') as csvfile:
             writer = csv.writer(csvfile,delimiter='\t', quotechar='\'',quoting=csv.QUOTE_MINIMAL)
-            writer.writerow( [ "dt.nsecs", "dt.secs", "position", "velocity", "velocity_ref", "displacement", "displacement_ref", "current","commanded_effort","analogIN0",'clutchState' ] )
-        self.bufferSize = 100
+            writer.writerow( [ "time.nsecs", "time.secs", "position", "velocity", "displacement", "reference", "current","commanded_effort","analogIN0",'clutchState' ] )
+        self.bufferSize = bufferSize
+        self.dataSize = 0
         self.count = 0
         self.buffer = []
+
+        rospy.init_node('logger',anonymous=True)
+        print "starting up logger ..."
+        self.subsC = rospy.Subscriber(rosNode,statusMessage,self.callback)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self,exc_type, exc_val, exc_tb):
+        print "#####################"
+        print "stopping logger ..."
+        self.dataSize += self.count
+        with open(self.fileName,'a') as csvfile:
+            writer = csv.writer(csvfile,delimiter='\t', quotechar='\'',quoting=csv.QUOTE_MINIMAL)
+            print '{:10s} {:10d}'.format('Final #Samples:',self.dataSize)
+            for row in self.buffer:
+                writer.writerow(row)
+        print " "
+        self.subsC.unregister()
+
+
     def callback(self,data):
-        self.buffer.append((data.dt.nsecs,data.dt.secs,data.position,data.velocity,data.velocity_ref,data.displacement,data.displacement_ref,data.current,data.commanded_effort,data.analogIN0,data.clutchState))
+        self.buffer.append((data.time.nsecs,data.time.secs,data.position,data.velocity,data.displacement,data.reference,data.current,data.commanded_effort,data.analogIN0,data.clutchState))
         self.count +=1
         if self.count > self.bufferSize:
             # reset counter
             self.count = 0
+            # add to total counter
+            self.dataSize += self.bufferSize
             # write new data and append it
             with open(self.fileName,'a') as csvfile:
                 writer = csv.writer(csvfile,delimiter='\t', quotechar='\'',quoting=csv.QUOTE_MINIMAL)
-                print "logging data..."
                 for row in self.buffer:
                     writer.writerow(row)
 
@@ -33,14 +99,14 @@ class simpleLogger:
                 self.buffer = []
 
 
-    def listener(self):
-        rospy.init_node('listener',anonymous=True)
-        print "starting up logger ..."
-        rospy.Subscriber("/myo/myo_muscle0_controller/DebugMessage",statusMessage,self.callback)
-        rospy.spin()
-
-
-
 if __name__ == '__main__':
-    a = simpleLogger()
-    a.listener()
+    if len(sys.argv) == 2:
+        name = sys.argv[1]
+    else:
+        print "Using standard name: data-[...]"
+        name = "data"
+
+    a = simpleLogger(name)
+
+    # keep process alive
+    rospy.spin()
