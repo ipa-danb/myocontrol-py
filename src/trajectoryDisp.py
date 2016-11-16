7f7bef2c1b39fedce3f171e8466cc69c48993e37
Daniel Bargmann <daniel.bargmann@ipa.fraunhofer.de>
2016-11-16 13:11:20 +0100

    reworked console output made logger optional added support for more commands

--- src/trajectoryDisp.py
+++ src/trajectoryDisp.py
@@ -12,11 +12,12 @@ class TrajectoryMover:
     def __init__(self,bufferSize=1000,fileName="traj_1",directory="scripts",logFileName=None):
         self.trajFileName = fileName
         self.maxLines = 0
-        self.checkFile()
         self.directory = directory
         self.bufferSize = bufferSize
-        if logFileName == None:
-            self.logFileName = fileName
+        self.commands = {"mov": ('c',(5,200),self.setReference), "clt" : ('s',(0,1),self.setClutch) }
+        self.logFileName = logFileName
+        self.checkFile()
+
 
     def setReference(self,ref):
         """Calls RosService /myo/myo_muscle0_controller/set_reference to set reference """
@@ -27,18 +28,30 @@ class TrajectoryMover:
         except rospy.ServiceException, e:
             print " "
 
+
     def checkFile(self):
         with open('scripts/' +self.trajFileName,'rb') as csvfile:
             trajReader = csv.reader(csvfile,delimiter=',',quotechar='|')
-            self.maxLines = sum(1 for row in trajReader)
             for row in trajReader:
-                if row[0] == 'clt':
-                    if not (int(row[1]) == 0 or int(row[1])== 1):
-                        raise Exception("Trajectory violated bounds at " + str(row))
-                else:
-                    row =  [int(row[0]), float(row[1])]
-                    if not (int(row[0]) > 0 and int(row[0]) < 300 and int(row[1]) > 0):
-                        raise Exception("Trajectory violated bounds at " + str(row))
+                # check if command is known
+                if not self.commands.has_key(row[0]):
+                    raise Exception("command unknown: " + str(row[0]) + " at Line " + str(trajReader.line_num))
+
+                # check if command is in bounds
+                bounds = self.commands.get(row[0])
+                if bounds[0] == 'c':
+                    if not ( float(row[1]) >= bounds[1][0] and float(row[1]) <= bounds[1][1] ):
+                        raise Exception("command not in bound: " + str(row) + " vs bounds " + str(bounds[1][0:2]) + " in Line " + str(trajReader.line_num) )
+                elif bounds[0] == 's':
+                    if not ( float(row[1]) in bounds[1] ):
+                        raise Exception("command not accepted value: " + str(row[1]) + " not in " + str(bounds[1]) + " in Line " + str(trajReader.line_num) )
+
+                # check if time is in bound
+                if len(row) > 2:
+                    if (float(row[2]) < 0):
+                        raise Exception("wait time lower than 0 in Line: " + str(trajReader.line_num))
+            self.maxLines = trajReader.line_num
+
 
     def setClutch(self,state):
         rospy.wait_for_service('/myo/myo_muscle0_controller/set_clt')
@@ -48,35 +61,46 @@ class TrajectoryMover:
         except rospy.ServiceException, e:
             sys.exit("Error! Cant Set Clutch")
 
+
     def setTrajectoryFile(self, filename):
         self.trajFileName = filename
         self.checkFile()
 
+    def traj(self):
+        print " "
+        print "###############################"
+        print "Starting to drive trajectory..."
+        print "###############################\n"
+        with open(self.directory + '/' +self.trajFileName,'rb') as csvfile:
+            trajReader = csv.reader(csvfile,delimiter=',',quotechar='|')
+            for row in trajReader:
+                print('\rLines: {:4d} / {:4d} : {:20s}'.format(trajReader.line_num,self.maxLines,str(row[0]) + " : " + str(row[1]) )),
+                sys.stdout.flush()
+                self.commands.get(row[0])[2](float(row[1]))
+                if len(row) > 2:
+                    time.sleep(float(row[2]))
+        print "\n"
+        print "###############################"
+        print "finished with trajectory"
+        print "###############################"
+        print "\n"
+
     def driveTrajectory(self):
         self.checkFile()
+        print "\n"
+        if self.logFileName == None:
+            print "###############################"
+            print "No logger specified ..."
+            print "###############################"
+            self.traj()
+        else:
+            print "###############################"
+            print "setting up logger with file " + self.logFileName + " ..."
+            print "###############################"
+            with logger.simpleLogger(self.logFileName,self.bufferSize) as log:
+                self.traj()
 
 
-        print "###############################"
-        print "setting up logger with file " + name + " ..."
-        with logger.simpleLogger(self.logFileName,self.bufferSize) as log:
-            print "starting to drive trajectory..."
-            print " "
-            print "###############################"
-            print "Transitions"
-            print "###############################"
-            with open(self.directory + '/' +self.trajFileName,'rb') as csvfile:
-                trajReader = csv.reader(csvfile,delimiter=',',quotechar='|')
-                for row in trajReader:
-                    print('\rLines: {:4d} / {:4d} : {:20s}'.format(trajReader.line_num,self.maxLines,str(row) )),
-                    sys.stdout.flush()
-                    if row[0] == 'clt':
-                        self.setClutch(int(row[1]))
-                    else:
-                        #print "Ref: " + str(row[0])
-                        self.setReference(int(row[0]))
-                        time.sleep(float(row[1]))
-            print "\n"
-            print "finished with trajectory"
 
     def changeDirectory(self,directory):
         self.directory = directory
@@ -107,5 +131,5 @@ if __name__ == '__main__':
         elif opt in ("-t", "--trajectoryFile"):
             trajFileName = str(arg)
 
-    t = TrajectoryMover(bufferSize=bufferSize,fileName=trajFileName)
+    t = TrajectoryMover(bufferSize=bufferSize,fileName=trajFileName,logFileName=name )
     t.driveTrajectory()
