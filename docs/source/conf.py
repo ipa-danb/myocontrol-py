6a91ab195689795df841002faaa2e86a5ddc3986
Daniel Bargmann <daniel.bargmann@ipa.fraunhofer.de>
2016-11-17 18:14:46 +0100

    updated requirements v2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -350,5 +350,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['rospy']
+MOCK_MODULES = ['rospy','pyside']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
